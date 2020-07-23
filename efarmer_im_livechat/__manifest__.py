d7228cb39d168741e7100735461d8b4eff4c7cb3
woopzzz <woopzzz.job@gmail.com>
2020-07-23 16:13:21 +0000

    S-E-8 - live chat styling (on external websites)

---
+++ efarmer_im_livechat/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Live Chat, eFarmer',
+
+    'version': '1.0',
+    'category': 'Other',
+    'author': 'ERP Ukraine',
+    'website': 'https://erp.co.ua',
+    'support': 'support@erp.co.ua',
+    'license': 'OPL-1',
+    'auto_install': False,
+    'installable': True,
+    'application': True,
+
+    'demo': [],
+
+    'depends': [
+        'im_livechat',
+    ],
+
+    'data': [
+        'views/templates.xml',
+    ],
+}
