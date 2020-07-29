255a8bad66e43ff1e4a8e04afdc30496aa2119ba
woopzzz <woopzzz.job@gmail.com>
2020-07-29 12:04:09 +0000

    S-E-10 - l10n_nl translations addon

---
+++ efarmer_l10n_nl/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': 'Netherlands - Accounting, eFarmer',
+    'version': '1.0',
+    'summary': 'Translations for the NL chart of accounts.',
+
+    'category': 'Other',
+    'author': 'ERP Ukraine',
+    'website': 'https://erp.co.ua',
+    'support': 'support@erp.co.ua',
+    'license': 'OPL-1',
+    'auto_install': False,
+    'application': False,
+    'installable': True,
+
+    'demo': [],
+    'data': [],
+
+    'depends': [
+        'l10n_nl',
+    ],
+}
