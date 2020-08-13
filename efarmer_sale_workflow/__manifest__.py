933266f0b8da2de14282accd48820c19600eeb3b
woopzzz <woopzzz.job@gmail.com>
2020-08-13 10:36:40 +0000

    S-E-11 Commercial Invoice

--- efarmer_sale_workflow/__manifest__.py
+++ efarmer_sale_workflow/__manifest__.py
@@ -1,7 +1,7 @@
 {
     'name': 'Sale Workflow, eFarmer',
 
-    'version': '1.0',
+    'version': '1.1',
     'category': 'Other',
     'author': 'ERP Ukraine',
     'website': 'https://erp.co.ua',
@@ -15,12 +15,15 @@
 
     'depends': [
         'base_automation',
+        'web',
         'mail',
+        'sale',
         'sale_crm',
         'sale_stock',
     ],
 
     'data': [
+        'reports/commercial_invoice.xml',
         'views/ir_actions_server.xml',
     ],
 }
