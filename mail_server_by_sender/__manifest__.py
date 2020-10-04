e1c8d3e18ffacbcf2a20942ebbf90ef4b019fb0f
Bohdan Lisnenko <lem8r.ka11a@gmail.com>
2020-10-04 15:45:43 +0300

    add mail_server_by_sender

---
+++ mail_server_by_sender/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Mail Server by Sender',
+    'version': '1.1',
+    'category': 'Tools',
+    'author': 'ERP Ukraine',
+    'website': 'https://erp.co.ua',
+    'support': 'support@erp.co.ua',
+    'license': 'OPL-1',
+    'summary': 'Select Mail Server by Sender address.',
+    'description': """
+Select Mail Server by Sender address.
+""",
+    'depends': ['base', 'mail'],
+    'installable': True,
+    'auto_install': False
+}
