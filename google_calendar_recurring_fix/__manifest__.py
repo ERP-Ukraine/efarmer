625d8721fcf053c94a4eb89a164d67391642c618
Bohdan Lisnenko <lem8r.ka11a@gmail.com>
2020-10-04 15:45:32 +0300

    add google_calendar_recurring_fix

---
+++ google_calendar_recurring_fix/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Google Calendar Recurring Fix',
+    'description': """
+Fix problem when syncing recurring events
+=========================================
+
+Can be removed when [PR 52989](https://github.com/odoo/odoo/pull/52989) will be merged.
+
+""",
+    'version': '1.1',
+    'category': 'Tools',
+    'installable': True,
+    'auto_install': True,
+    'depends': ['google_calendar']
+}
