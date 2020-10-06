c4ba950db611d04afb8304eaee4ba15bf9b66add
woopzzz <woopzzz.job@gmail.com>
2020-10-06 11:28:34 +0000

    S-E-45 - Поиск истории общения: перенести меню

--- efarmer_conversation_history/__manifest__.py
+++ efarmer_conversation_history/__manifest__.py
@@ -1,7 +1,7 @@
 {
     'name': 'Conversation History, eFarmer',
 
-    'version': '1.3',
+    'version': '1.4',
     'category': 'Other',
     'author': 'ERP Ukraine',
     'website': 'https://erp.co.ua',
