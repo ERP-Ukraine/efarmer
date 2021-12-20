db31ac33f1fb71d41bc694bc55a173b78c080858
Bohdan Lisnenko <lem8r.ka11a@gmail.com>
2021-12-20 12:24:10 +0000

    [NEW] mrp_account: Kit's revaluation Do not create account moves when updating cost from BoM
    S-E-81

---
+++ efarmer_mrp_account/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'eFarmer MRP Account',
+    'version': '1.0',
+    'category': 'Manufacturing',
+    'author': 'ERP Ukraine',
+    'website': 'https://erp.co.ua',
+    'support': 'support@erp.co.ua',
+    'license': 'OPL-1',
+    'depends': ['mrp_account'],
+    'description': "",
+    'data': [
+        # 'views/mrp_bom_views.xml',
+        # 'views/mrp_production_views.xml',
+        # 'views/stock_picking_views.xml',
+        # 'views/stock_move_line_views.xml',
+    ],
+    'auto_install': False,
+    'installable': True,
+    'application': False,
+}
