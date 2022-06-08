27ccc1e0eb339ec5adcca8a25fcce7846941a97b
woopzz <bkovtunovych@gmail.com>
2022-06-08 10:43:12 +0000

    S-E-89 Visible "Inventory".
    Make the "Inventory" group be visible
    on the contact form view.

---
+++ efarmer_stock/__manifest__.py
@@ -0,0 +1,23 @@
+{
+    'name': 'Stock, eFarmer',
+
+    'version': '1.0',
+    'category': 'Other',
+    'author': 'ERP Ukraine LLC',
+    'website': 'https://erp.co.ua',
+    'support': 'support@erp.co.ua',
+    'license': 'LGPL-3',
+    'auto_install': False,
+    'installable': True,
+    'application': True,
+
+    'demo': [],
+
+    'depends': [
+        'stock',
+    ],
+
+    'data': [
+        'views/res_partner_views.xml',
+    ],
+}
