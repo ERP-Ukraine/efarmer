a0e35c5294c131800a55cff4b2aa0aac4af8ba47
woopzz <bkovtunovych@gmail.com>
2022-07-14 20:33:48 +0000

    S-E-87 [PORT] mail_server_by_sender
    Port to v15.

--- mail_server_by_sender/models/ir_mail_server.py
+++ mail_server_by_sender/models/ir_mail_server.py
@@ -9,8 +9,9 @@ class IrMailServer(models.Model):
 
     @api.model
     def send_email(self, message, mail_server_id=None, smtp_server=None, smtp_port=None,
-                   smtp_user=None, smtp_password=None, smtp_encryption=None, smtp_debug=False,
-                   smtp_session=None):
+                   smtp_user=None, smtp_password=None, smtp_encryption=None,
+                   smtp_ssl_certificate=None, smtp_ssl_private_key=None,
+                   smtp_debug=False, smtp_session=None):
         if not mail_server_id:
             # If there is outgoing mail server with username equals to sender's address - use it!
             smtp_from = (message['Return-Path'] or self._get_default_bounce_address() or
@@ -18,7 +19,7 @@ class IrMailServer(models.Model):
             sender_address = extract_rfc2822_addresses(smtp_from)[:0]
             if sender_address:
                 mail_server_id = self.sudo().search([('smtp_user', '=', sender_address)], limit=1)
-        return super().send_email(message, mail_server_id=mail_server_id, smtp_server=smtp_server,
-                                  smtp_port=smtp_port, smtp_user=smtp_user,
-                                  smtp_password=smtp_password, smtp_encryption=smtp_encryption,
-                                  smtp_debug=smtp_debug, smtp_session=smtp_session)
+        return super().send_email(message, mail_server_id=mail_server_id, smtp_server=smtp_server, smtp_port=smtp_port,
+                   smtp_user=smtp_user, smtp_password=smtp_password, smtp_encryption=smtp_encryption,
+                   smtp_ssl_certificate=smtp_ssl_certificate, smtp_ssl_private_key=smtp_ssl_private_key,
+                   smtp_debug=smtp_debug, smtp_session=smtp_session)
