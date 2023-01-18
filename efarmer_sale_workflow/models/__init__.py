6ac11f1ccdf5c6860361653085993da04815e283
Alexander Protasevich <alexander.p@ventor.tech>
2023-01-18 13:51:18 +0400

    [ADD] EF-46:add product label template

--- efarmer_sale_workflow/models/__init__.py
+++ efarmer_sale_workflow/models/__init__.py
@@ -1,2 +1,2 @@
 from . import (account_invoice, efarmer_client_type, product_category,
-               product_template, res_partner, sale_order, stock_picking)
+               product_template, res_partner, sale_order, stock_picking, ir_actions_report)
