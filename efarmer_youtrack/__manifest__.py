ff98be86a8c6e64f9530ddd6b791d3a4d49ffdd4
Alexander Protasevich <alexander.p@ventor.tech>
2023-01-24 18:26:58 +0400

    [ADD] EF-53: create settings and youtrack operations wizard

---
+++ efarmer_youtrack/__manifest__.py
@@ -0,0 +1,31 @@
+# Copyright 2023 VentorTech OU
+# See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Odoo YouTrack Integration',
+    'summary': """
+        Integration between Odoo and YouTrack system
+    """,
+    'version': '15.0.0.1.0',
+    'category': 'Other',
+    # "images": ["static/description/images/logo.gif"],
+    'author': 'VentorTech',
+    'website': 'https://ventor.tech',
+    'license': 'OPL-1',
+    'depends': [
+        'project',
+    ],
+    'data': [
+        # Security
+        # 'security/security.xml',
+        'security/ir.model.access.csv',
+        # Initial Data
+        # 'data/ir_actions_server_data.xml',
+        # Wizards
+        'wizard/youtrack_operations_wizard.xml',
+        # Model Views
+        'views/res_config_settings_views.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
---
+++ efarmer_youtrack/wizard/__init__.py
@@ -0,0 +1,1 @@
+from . import youtrack_operations_wizard
