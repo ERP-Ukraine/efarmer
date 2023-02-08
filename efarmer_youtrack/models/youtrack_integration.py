4b704fbeced6c215b4a7112b83782f79dace69d2
Alexander Protasevich <alexander.p@ventor.tech>
2023-02-08 19:35:00 +0400

    [REF] EF-53: refactor functionality of importing employees and tasks

--- efarmer_youtrack/models/youtrack_integration.py
+++ efarmer_youtrack/models/youtrack_integration.py
@@ -10,13 +10,6 @@ from odoo.exceptions import ValidationError
 
 
 PROJECT_KEY = 'shortName'
-CUSTOM_FIELDS_MAP = {
-    'Estimation': 'planned_hours',
-    'Type': 'issue_type_id',
-    'Product version': 'product_version_id',
-    'Name PL': 'name_pl',
-    'Product': 'product_id',
-    }
 
 
 class YoutrackIntegration(models.Model):
@@ -110,108 +103,93 @@ class YoutrackIntegration(models.Model):
         res = [project for project in ext_projects if project[PROJECT_KEY] == self.project_code]
         return res
 
-    def _get_project_by_code(self, project_code):
-        project = self.env['project.project'].search([
-            ('project_code', '=', project_code)
-        ], limit=1)
-        return project or False
-
-    def _get_obj_by_ext_id(self, model, value, operator='=', limit=1):
-        obj = self.env[model].search([('youtrack_id', operator, value)], limit=limit)
+    def _get_object(self, model, field, value, operator='=', limit=None, check_unique=False):
+        obj = self.env[model].search([(field, operator, value)], limit=limit)
+        if check_unique:
+            self._check_unique(obj, field)
         return obj
 
-    def _get_obj_by_name(self, model, value, operator='=', limit=1):
-        obj = self.env[model].search([('name', operator, value)], limit=limit)
+    def _check_unique(self, object, field):
+        if len(object) > 1:
+            raise ValidationError(_(
+                'There are more than one objects of model {} with the same {}!!'
+            ).format(object[0]._name, field))
+        return True
+
+    def _create_object(self, model, vals):
+        obj = self.env[model].create(vals)
         return obj
 
     def _get_ts_to_create(self, ext_ts):
-        exist_ts = self._get_obj_by_ext_id('account.analytic.line', False, operator='!=', limit=None)
+        exist_ts = self._get_object('account.analytic.line', 'youtrack_id', False, operator='!=')
         exist_ts_ext_ids = exist_ts.mapped('youtrack_id')
-        exist_projects = self._get_obj_by_ext_id('project.project', False, operator='!=', limit=None)
+        exist_projects = self._get_object('project.project', 'youtrack_id', False, operator='!=')
         exist_project_ext_ids = exist_projects.mapped('youtrack_id')
 
         ts_to_create = [ts for ts in ext_ts if ts['id'] not in exist_ts_ext_ids
                         and ts['issue']['project']['id'] in exist_project_ext_ids]
         return ts_to_create
 
-    def _get_issue_type(self, ext_issue_type):
-        issue_type = self._get_obj_by_name('youtrack.issue.type', ext_issue_type['name'])
-        if not issue_type:
-            issue_type = self.env['youtrack.issue.type'].create({
-                'name': ext_issue_type['name'] or '',
-                'youtrack_id': ext_issue_type['id'],
-            })
-        return issue_type.id
-
-    def _get_product_version(self, ext_product_version):
-        product_version = self._get_obj_by_name(
-            'youtrack.product.version',
-            ext_product_version['name'],
-        )
-        if not product_version:
-            product_version = self.env['youtrack.product.version'].create({
-                'name': ext_product_version['name'] or '',
-                'youtrack_id': ext_product_version['id'],
-            })
-        return product_version.id
-
-    def _get_product(self, ext_product):
-        product = self._get_obj_by_name('account.asset', ext_product['name'])
-        if not product:
-            product = self.env['account.asset'].create({
-                'name': ext_product['name'] or '',
-                'youtrack_id': ext_product['id'],
-            })
-        return product.id
-
-    def _get_work_type(self, vals):
-        if not vals.get('type', False):
-            return None
-        work_type = self._get_obj_by_ext_id('youtrack.work.type', vals['type']['id'])
-        if not work_type:
-            work_type = self.env['youtrack.work.type'].create({
-                'youtrack_id': vals['type']['id'],
-                'name': vals['type']['name'] or '',
-            })
-
-        return work_type.id
+    def _get_object_by_name(self, model, custom_data):
+        obj = self._get_object(model, 'name', custom_data['name'], check_unique=True)
+        if not obj:
+            vals = {
+                'name': custom_data['name'] or '',
+                'youtrack_id': custom_data['id'],
+            }
+            obj = self._create_object(model, vals)
+        return obj
 
     def _minutes_to_hours(self, minutes):
         hours = float(minutes // 60 + minutes % 60 / 60)
         return hours
 
     def _get_api_customs_values(self, vals):
+        CUSTOM_FIELDS_MAP = {
+            'Estimation': ('planned_hours', None),
+            'Type': ('issue_type_id', 'youtrack.issue.type'),
+            'Product version': ('product_version_id', 'youtrack.product.version'),
+            'Name PL': ('name_pl', None),
+            'Product': ('product_id', 'account.asset'),
+        }
+
         custom_values = {}
         for field in vals['customFields']:
-            if field.get('name') in CUSTOM_FIELDS_MAP:
-                value = field['value']
-                custom_values[CUSTOM_FIELDS_MAP.get(field['name'])] = value if value else None
-
-        if custom_values.get('issue_type_id', False):
-            ext_issue_type = custom_values['issue_type_id']
-            custom_values['issue_type_id'] = self._get_issue_type(ext_issue_type)
-
-        if custom_values.get('product_version_id', False):
-            ext_product_version = custom_values['product_version_id']
-            custom_values['product_version_id'] = self._get_product_version(ext_product_version)
-
-        if custom_values.get('product_id', False):
-            ext_product = custom_values['product_id']
-            custom_values['product_id'] = self._get_product(ext_product)
+            field_name = field.get('name')
+            if field_name in CUSTOM_FIELDS_MAP:
+                custom_data = field['value']
+                odoo_field, model = CUSTOM_FIELDS_MAP.get(field_name)
+                if not model:
+                    custom_values[odoo_field] = custom_data
+                else:
+                    custom_values[odoo_field] = self._get_object_by_name(model, custom_data).id \
+                        if custom_data else None
 
         if custom_values.get('planned_hours', False):
-            estimation_minutes = int(custom_values['planned_hours']['minutes'])
-            planned_hours = self._minutes_to_hours(estimation_minutes)
+            planned_hours = self._minutes_to_hours(custom_values['planned_hours']['minutes'])
             custom_values['planned_hours'] = planned_hours
 
         return custom_values
 
-    def _get_employees_data(self, ext_employees):
-        employees = self.env['hr.employee'].search([])
-        emails = {employee.work_email: employee for employee in employees if employee.work_email}
-        exist_employees_ext_ids = employees.mapped('youtrack_id')
-        to_do_employees = [employee for employee in ext_employees if employee['id'] not in exist_employees_ext_ids]
-        return emails, to_do_employees
+    def _get_employees_data(self, ext_empls):
+        update_data = {}
+        # find employees in Odoo with emails from YouTrack
+        ext_emails = [employee['email'] for employee in ext_empls]
+        empls = self.env['hr.employee'].search([('work_email', 'in', ext_emails)])
+        empls_emails = empls.mapped('work_email')
+        # define create list of YouTrack employees which have
+        # email and don't exist in Odoo
+        empls_to_create = [empl for empl in ext_empls
+                           if empl['email'] and empl['email'] not in empls_emails]
+
+        # check if there are employees in Odoo without youtrack_id
+        # and prepare data for updating them
+        empls_no_ext_ids = empls.filtered(lambda x: not x.youtrack_id)
+        if empls_no_ext_ids:
+            empls_no_ext_ids = {empl.work_email: empl for empl in empls_no_ext_ids}
+            update_data = {empl['id']: empls_no_ext_ids.get(empl['email']) for empl in ext_empls
+                           if empl['email'] in empls_no_ext_ids}
+        return empls_to_create, update_data
 
     def _timestamp_to_date(self, timestamp):
         if len(str(timestamp)) == 13:
@@ -230,7 +208,12 @@ class YoutrackIntegration(models.Model):
         return True
 
     def _create_task(self, vals):
-        project = self._get_obj_by_ext_id('project.project', vals['project']['id'])
+        project = self._get_object(
+            'project.project',
+            'youtrack_id',
+            vals['project']['id'],
+            check_unique=True,
+        )
         custom_values = self._get_api_customs_values(vals)
 
         new_vals = {
@@ -252,8 +235,14 @@ class YoutrackIntegration(models.Model):
         return new_task
 
     def _create_ts(self, vals, employee):
-        task = self._get_obj_by_ext_id('project.task', vals['issue']['id'])
-        work_type_id = self._get_work_type(vals)
+        task = self._get_object(
+            'project.task',
+            'youtrack_id',
+            vals['issue']['id'],
+            check_unique=True,
+        )
+        work_type_id = self._get_object_by_name('youtrack.work.type', vals['type']).id \
+            if vals.get('type') else None
         date = self._timestamp_to_date(vals.get('date'))
 
         self.env['account.analytic.line'].create({
@@ -291,23 +280,24 @@ class YoutrackIntegration(models.Model):
 
     def _validate_project_request(self):
         if self.project_code:
-            if self._get_project_by_code(self.project_code):
+            if self._get_object('project.project', 'project_code', self.project_code, limit=1):
                 raise ValidationError(_(
                     'Project with code "{}" already exists in Odoo.'
-                    ).format(self.project_code))
+                ).format(self.project_code))
         else:
             raise ValidationError(_(
-                    'You need to set Project Code for importing Project.'
-                    ))
+                'You need to set Project Code for importing Project.'
+            ))
 
     def _validate_work_items_request(self):
         if not self.date_from:
-            raise ValidationError(_('You need to set Start Date  '
-                                    'for importing Work Items.'))
+            raise ValidationError(_(
+                'You need to set Start Date for importing Work Items.'
+            ))
 
     def api_get_project(self, project_code):
         get_project_url = 'admin/projects?fields=name,shortName,'\
-                          'id&$top=1000000&query={}'.format(project_code)
+                          'id&$top=100000&query={}'.format(project_code)
         ext_project = self._send_youtrack_request(get_project_url) or []
         if ext_project:
             # we can't receive exact response with such request,
@@ -320,23 +310,31 @@ class YoutrackIntegration(models.Model):
         get_employee_url = "users?fields=id,fullName,email&$top=100000"
         ext_employees = self._send_youtrack_request(get_employee_url) or []
         if ext_employees:
-            # write youtrack_id if employee with imported email exists in Odoo,
-            # otherwise create employee
-            emails, to_do_employees = self._get_employees_data(ext_employees)
-            for employee in to_do_employees:
-                ext_email = employee.get('email', False)
-                if ext_email in emails:
-                    odoo_employee = emails[ext_email]
-                    odoo_employee.youtrack_id = employee.get('id', None)
-                else:
+            # create employee if employee with imported email doesn't exists
+            # in Odoo, otherwise write youtrack_id if it's empty
+            empls_to_create, update_data = self._get_employees_data(ext_employees)
+
+            if empls_to_create:
+                for employee in empls_to_create:
                     self._create_employee(employee)
 
+            if update_data:
+                for youtrack_id, employee in update_data.items():
+                    employee.youtrack_id = youtrack_id
+
     def api_get_task(self, task_ext_id, integration_id, child=None):
-        task = self._get_obj_by_ext_id('project.task', task_ext_id)
+        """
+        Check if task exists in Odoo, otherwise get it from
+        integration and repeat this operation recursively
+        as long as the task has a parent.
+        After each recursive execution create link between parent
+        and child tasks.
+        """
+        task = self._get_object('project.task', 'youtrack_id', task_ext_id, check_unique=True)
         if task:
             if child:
                 child.parent_id = task.id
-            return task
+            return True
 
         get_tasks_url = 'issues/{}?fields=id,idReadable,summary,project(id),'\
                         'parent(issues(id,project(id))),customFields(name,'\
@@ -354,14 +352,20 @@ class YoutrackIntegration(models.Model):
             if parent_task:
                 parent_id = parent_task[0]['id']
                 parent_project_id = parent_task[0]['project']['id']
-                parent_project = self._get_obj_by_ext_id('project.project', parent_project_id)
+                parent_project = self._get_object(
+                    'project.project',
+                    'youtrack_id',
+                    parent_project_id,
+                    check_unique=True,
+                )
+                # create task if project exists in Odoo
                 if parent_project:
                     self.api_get_task(parent_id, integration_id, child=task)
-            return task
+            return True
 
     def api_get_work_items(self, start, integration_id):
         # import work items (timesheets) for all existing employees with yourack_id
-        employees = self._get_obj_by_ext_id('hr.employee', False, operator='!=', limit=None)
+        employees = self._get_object('hr.employee', 'youtrack_id', False, operator='!=')
         for employee in employees:
             get_ts_url = 'workItems?fields=date,duration(minutes),author(id),text,'\
                          'type,id,type(id,name),issue(id,project(id))&startDate={}'\
@@ -371,7 +375,6 @@ class YoutrackIntegration(models.Model):
                 # filter out existing ts and ts with project, that doesn't exist in Odoo
                 ts_to_create = self._get_ts_to_create(ext_ts)
                 for ts in ts_to_create:
-                    # TODO: run api_get_task with_delay() ??
                     self.api_get_task(ts['issue']['id'], integration_id)
                     self._create_ts(ts, employee)
         # create links for all childs to its epic task
