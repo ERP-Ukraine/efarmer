31cae23cdac0deb292b2728405b61f1089f97ef7
denisventor <denis.b@ventor.tech>
2023-02-26 15:35:30 +0100

    [IMP] EF-83: ADD MODULE

---
+++ efarmer_capitalization/models/project_capitalization.py
@@ -0,0 +1,148 @@
+# Copyright 2023 VentorTech OU
+# Part of Ventor modules. See LICENSE file for full copyright and licensing details.
+from odoo import models, fields
+
+
+class ProjectCapitalization(models.Model):
+    _name = 'project.capitalization'
+    _description = 'Project Capitalization'
+    _check_company_auto = True
+
+    name = fields.Char(
+        string='Name',
+        default=lambda self: self.env['ir.sequence'].next_by_code('project.capitalization.sequence'),
+        readonly=True,
+    )
+
+    status = fields.Selection(
+        [('new', 'New'),
+         ('in_progress', 'In Progress'),
+         ('done', 'Done')],
+        string='Status',
+        required=True,
+        default='new',
+        tracking=True,
+    )
+    start_date = fields.Date(
+        string='Start Date',
+        required=True,
+        default=fields.Date.context_today,
+        tracking=True
+    )
+    end_date = fields.Date(
+        string='End Date',
+        required=True,
+        tracking=True
+    )
+    company_id = fields.Many2one(
+        'res.company',
+        'Company',
+        required=True,
+        index=True,
+        default=lambda self: self.env.company,
+    )
+    work_types = fields.Many2many(
+        'account.analytic.tag',
+        'project_capitalization_work_types_rel',
+        'capitalization_id',
+        'tag_id',
+        string='Work Types',
+    )
+    account_asset_counterpart_id = fields.Many2one(
+        'account.account',
+        string='Account Asset Counterpart',
+        check_company=True,
+        domain="[('internal_type','=','other'), ('deprecated', '=', False), ('company_id', '=', company_id)]",
+        help="Account used as counterpart for entries related to this asset.",
+        required=True,
+        tracking=True
+    )
+    capitalization_line_ids = fields.One2many(
+        'project.capitalization.line',
+        'capitalization_id',
+        string='Capitalize To',
+    )
+    analytic_line_ids = fields.Many2many(
+        'account.analytic.line',
+        string='Analytic Line',
+    )
+
+    def generate_report(self):
+        self.ensure_one()
+        domain = [
+            ('date', '>=', self.start_date),
+            ('date', '<=', self.end_date),
+            ('task_product_id', '!=', False),
+            ('is_capitalized', '=', False),
+            ('task_id.analytic_tag_ids', 'in', self.work_types.ids),
+        ]
+        grouped_data = self.env['account.analytic.line'].read_group(
+            domain,
+            ['task_product_id', 'unit_amount', 'amount'],
+            ['task_product_id'],
+        )
+
+        capitalized_lines = []
+        analytic_line_ids = self.env['account.analytic.line'].search(domain)
+        self.env['project.capitalization.line'].search([('capitalization_id', '=', self.id)]).unlink()
+        for data in grouped_data:
+            task_product_id = data['task_product_id'][0]
+            hours_spent = data['unit_amount']
+            amount = data['amount']
+            capitalized_lines.append((0, 0, {
+                'account_asset_counterpart_id': self.account_asset_counterpart_id.id,
+                'hours_spent': hours_spent,
+                'amount': amount,
+                'capitalization_id': self.id,
+                'asset_id': task_product_id,
+            }))
+        self.write({
+            'analytic_line_ids': analytic_line_ids,
+            'capitalization_line_ids': capitalized_lines,
+            'status': 'in_progress',
+        })
+
+    def capitalization(self):
+        self.line_capitalize()
+        lines = self.capitalization_line_ids
+        for line in lines:
+            asset_child = line.asset_id.env['account.asset'].create({
+                'parent_id': line.asset_id.id,
+                'state': 'open',
+                'name': f"{self.name} {fields.Date.today()}",
+                'method_number': line.asset_id.method_number,
+                'original_value': line.amount,
+                'journal_id': line.asset_id.journal_id.id,
+                'account_asset_id': line.account_asset_counterpart_id.id,
+                'account_depreciation_id': line.asset_id.account_depreciation_id.id,
+                'account_depreciation_expense_id': line.asset_id.account_depreciation_expense_id.id,
+            })
+            asset_child.compute_depreciation_board()
+        self.status = 'done'
+
+    def line_capitalize(self):
+        for capitalization in self:
+            domain = [
+                ('date', '>=', capitalization.start_date),
+                ('date', '<=', capitalization.end_date),
+                ('task_product_id', '!=', False),
+                ('is_capitalized', '=', False),
+                ('task_id.analytic_tag_ids', 'in', capitalization.work_types.ids),
+            ]
+            capitalization.env['account.analytic.line'].search(domain).update({'is_capitalized': lambda is_capitalized: True})
+
+    def open_analytic_lines(self):
+        analytic_lines = self.env['account.analytic.line'].search([('id', 'in', self.analytic_line_ids.ids)])
+        action = self.env.ref('analytic.account_analytic_line_action').read()[0]
+        action['domain'] = [('id', 'in', analytic_lines.ids)]
+        action['context'] = {'group_by': 'task_product_id'}
+        return action
+
+
+class Task(models.Model):
+    _inherit = 'project.task'
+
+    asset_id = fields.Many2one(
+        comodel_name='account.asset',
+        string='Product',
+    )
