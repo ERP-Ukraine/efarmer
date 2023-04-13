439ee78c4df66b3bd9847a969fef586363405301
denisventor <denis.b@ventor.tech>
2023-04-13 12:17:40 +0200

    [IMP] EF-123: add functionality v1.3

--- efarmer_sale_workflow/__manifest__.py
+++ efarmer_sale_workflow/__manifest__.py
@@ -46,6 +46,7 @@
         'views/efarmer_sale_workflow_menus.xml',
         'views/product_template_views.xml',
         'views/sale_order_views.xml',
+        'views/stock_warehouse_views.xml',
         'wizards/product_variant_replacement_wizard.xml',
     ],
 
