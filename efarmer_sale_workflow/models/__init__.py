2137d7d821d182d93e1355a0296fd5183f77221d
NadyaGornostay <chamamilla@tut.by>
2024-03-05 14:40:52 +0100

    [IMP] Logic to update description in sale order lines was added

--- efarmer_sale_workflow/models/__init__.py
+++ efarmer_sale_workflow/models/__init__.py
@@ -3,7 +3,6 @@ from . import account_intrastat_report
 from . import delivery_terms
 from . import efarmer_client_type
 from . import product_category
-from . import product_product
 from . import product_template
 from . import res_partner
 from . import sale_order
