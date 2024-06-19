7ed935be958cb30892973fe1f657ca5cda41aa6e
NadyaGornostay <chamamilla@tut.by>
2024-06-19 16:00:39 +0200

    [ADD] module to correct standard price computation was added

---
+++ efarmer_standard_price_correction/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2024 VentorTech OU
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+{
+    'name': 'eFarmer Product Cost Correction',
+    'version': '1.0',
+    'category': 'Hidden',
+    'author': 'VentorTech',
+    'website': 'https://ventor.tech',
+    'license': 'LGPL-3',
+    'depends': [
+        'product',
+        'stock_account',
+    ],
+    'description': "",
+    'data': [
+    ],
+    'auto_install': False,
+    'installable': True,
+    'application': True,
+}
