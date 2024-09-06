10d07a1c93943afc7acb8ce2f601b7ec5764a80d
nadezhda.gornostay <nadezhda.gornostay@ventor.tech>
2024-09-06 08:02:57 +0200

    [ADD] module account_journal_general_sequence was added

---
+++ efarmer_activities/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+{
+    "name": "CMI Activities",
+    "version": "1.1",
+    "category": "Custom",
+    "author": "VentorTech",
+    "website": "https://ventor.tech",
+    "support": "support@ventor.tech",
+    "license": "OPL-1",
+    "summary": "Manage your activities",
+    "depends": [
+        "mail",
+    ],
+    "data": [
+        "views/mail_activity_views.xml",
+    ],
+    "installable": True,
+    "application": True,
+}
+
