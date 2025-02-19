b453a309dc89c7b567ea7684fec6f3120b99f693
edward_yoch <edward.y@ventor.tech>
2025-02-19 14:15:23 +0000

    [IMP][EF-355] Added tracking to efarmer_client_type field

--- efarmer_sale_workflow/models/res_partner.py
+++ efarmer_sale_workflow/models/res_partner.py
@@ -4,4 +4,8 @@ from odoo import fields, models
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
-    efarmer_client_type = fields.Many2one('efarmer.client.type', 'Client Type')
+    efarmer_client_type = fields.Many2one(
+        comodel_name='efarmer.client.type',
+        string='Client Type',
+        tracking=True,
+    )
