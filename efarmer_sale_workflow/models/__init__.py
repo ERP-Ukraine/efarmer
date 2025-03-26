9d0b7711cf0d688d6c766a0783f36168a23bc0c2
edward_yoch <edward.y@ventor.tech>
2025-03-26 16:31:49 +0000

    [IMP][EF-364] Added description_label to product variant

--- efarmer_sale_workflow/models/__init__.py
+++ efarmer_sale_workflow/models/__init__.py
@@ -11,3 +11,4 @@ from . import sale_order_line
 from . import stock_picking
 from . import ir_actions_report
 from . import stock_warehouse
+from . import product_product
--- efarmer_sale_workflow/models/product_template.py
+++ efarmer_sale_workflow/models/product_template.py
@@ -6,7 +6,38 @@ from odoo.tools import config
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
-    description_label = fields.Text('Description for Product Labels', translate=True)
+    description_label = fields.Text(
+        string='Description for Product Labels',
+        compute='_compute_description_label',
+        inverse='_set_description_label',
+        store=True,
+        translate=True,
+    )
+
+    @api.depends('product_variant_ids', 'product_variant_ids.description_label')
+    def _compute_description_label(self):
+        unique_variants = self.filtered(lambda template: len(template.product_variant_ids) == 1)
+        for record in unique_variants:
+            translations = self.env['ir.translation'].search([
+                ('name', '=', 'product.product,description_label'),
+                ('type', '=', 'model'),
+                ('res_id', '=', record.product_variant_ids[:1].id),
+                ('state', '=', 'translated'),
+            ])
+            for translation in translations:
+                record.with_context(lang=translation.lang).description_label = translation.value
+
+    def _set_description_label(self):
+        for template in self:
+            if len(template.product_variant_ids) == 1:
+                translations = self.env['ir.translation'].search([
+                    ('name', '=', 'product.template,description_label'),
+                    ('type', '=', 'model'),
+                    ('res_id', '=', template.id),
+                    ('state', '=', 'translated'),
+                ])
+                for translation in translations:
+                    template.product_variant_ids.with_context(lang=translation.lang).description_label = translation.value
 
     @api.model
     def create(self, vals):
