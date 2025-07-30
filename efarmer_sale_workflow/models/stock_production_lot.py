59bb9ad847484666d05bd30ed816e4a66b6f9b3b
makmas <maksim.mashtaler@gmail.com>
2025-07-30 14:41:06 +0200

    [EF-381] Update after CR

--- efarmer_sale_workflow/models/stock_production_lot.py
+++ efarmer_sale_workflow/models/stock_production_lot.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class StockProductionLot(models.Model):
@@ -10,30 +10,28 @@ class StockProductionLot(models.Model):
         search='_search_is_unused',
         store=False
     )
-    
+
+    def _check_unused_lots(self):
+        lot_ids = self.ids
+        where = "lot_id IS NOT NULL" if not lot_ids else "lot_id IN %s"
+        params = () if not lot_ids else (tuple(lot_ids),)
+        self.env.cr.execute(f"""
+            SELECT DISTINCT lot_id
+            FROM stock_move_line
+            WHERE {where} AND state = 'done'
+        """, params)
+        return [r[0] for r in self.env.cr.fetchall()]
+
     def _compute_is_unused(self):
-        move_line_model = self.env['stock.move.line']
+        used_lot_ids = self._check_unused_lots()
         for lot in self:
-            # Check if this lot has ever been used in any move line
-            used = move_line_model.search_count([
-                ('lot_id', '=', lot.id),
-                ('state', '=', 'done')
-            ], limit=1)
-            lot.is_unused = not used
-    
+            lot.is_unused = lot.id not in used_lot_ids
+
+    @api.model
     def _search_is_unused(self, operator, value):
         if operator not in ('=', '!=') or not isinstance(value, bool):
             return []
-            
-        # Find all lots that appear in done move lines
-        self.env.cr.execute("""
-            SELECT DISTINCT lot_id 
-            FROM stock_move_line 
-            WHERE lot_id IS NOT NULL 
-            AND state = 'done'
-        """)
-        used_lot_ids = [r[0] for r in self.env.cr.fetchall()]
-        
+        used_lot_ids = self._check_unused_lots()
         if value:
             # Return lots NOT in the used list
             return [('id', 'not in', used_lot_ids)]
