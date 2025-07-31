afaa535fa63023cdd1705dac10bc85900a525cad
makmas <maksim.mashtaler@gmail.com>
2025-07-31 13:54:35 +0200

    [EF-383] Header fixes in Quotation/SO reports

---
+++ efarmer_sale_report_paperformat/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2025 VentorTech OU
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+{
+    'name': 'eFarmer Sale Report Paperformat',
+    'version': '1.0',
+    'category': 'Others',
+    'author': 'VentorTech',
+    'website': 'https://ventor.tech',
+    'license': 'LGPL-3',
+    'depends': [
+        'sale',
+    ],
+    'description': "",
+    'data': [
+        'reports/efarmer_paperformat_sale_report.xml',
+    ],
+
+    'installable': True,
+    'application': False,
+}
