07d3b83367f23737d3b3f5af1168ec18598cc36a
edward_yoch <edward.y@ventor.tech>
2025-10-30 16:38:49 +0000

    [ADD][EF-391] Added new fields

--- efarmer_account/__manifest__.py
+++ efarmer_account/__manifest__.py
@@ -2,17 +2,19 @@
 # Part of Ventor modules. See LICENSE file for full copyright and licensing details.
 {
     'name': 'Efarmer Account',
-    'version': '15.0.1.0.0',
+    'version': '15.0.1.1.0',
     'author': 'VentorTech',
     'website': 'https://ventor.tech/',
     'license': 'LGPL-3',
     'category': 'Accounting',
     'depends': [
         'account',
+        'hr',
     ],
     'description': '',
     'data': [
         'views/account_account_views.xml',
+        'views/account_bank_statement_views.xml',
     ],
     'installable': True,
     'application': False,
--- efarmer_account/models/__init__.py
+++ efarmer_account/models/__init__.py
@@ -1,3 +1,6 @@
 # Copyright 2025 VentorTech OU
 # Part of Ventor modules. See LICENSE file for full copyright and licensing details.
 from . import account_account
+from . import account_bank_statement_line
+from . import account_move
+from . import account_move_line
