fc8adc6502b09c621d91cbad8bc9ab043ff0ef08
NadyaGornostay <chamamilla@tut.by>
2026-02-12 11:01:12 +0000

    [EF-403][ADD] efarmer_sale_margin_mrp_account module

---
+++ efarmer_sale_margin_mrp_account/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "eFarmer Sale Marging for Kits",
+    "version": "19.0.1.0.0",
+    "category": "Manufacturing",
+    "author": "ERP Ukraine",
+    "website": "https://erp.co.ua",
+    "support": "support@erp.co.ua",
+    "license": "OPL-1",
+    "depends": ["sale_margin", "mrp_account"],
+    "description": "",
+    "data": [],
+    "auto_install": True,
+    "installable": True,
+    "application": False,
+}
---
+++ efarmer_sale_margin_mrp_account/models/sale_order.py
@@ -0,0 +1,39 @@
+from odoo import api, models
+
+
+class SaleOrderLine(models.Model):
+    _inherit = "sale.order.line"
+
+    def has_kit_product(self):
+        """Checks if current line has Kit Product"""
+        if not len(self) == 1 or not self.product_id:
+            return False
+        boms = self.env["mrp.bom"]._bom_find(
+            self.product_id, company_id=self.company_id.id, bom_type="phantom"
+        )
+        return bool(boms)
+
+    @api.depends("product_id", "company_id", "currency_id", "product_uom_id")
+    def _compute_purchase_price(self):
+        for line in self.filtered(lambda x: x.has_kit_product()):
+            line.product_id.button_bom_cost()
+        super()._compute_purchase_price()
+
+    def copy(self, default=None):
+        default = dict(default or {})
+        rec = super().copy(default)
+        if rec.has_kit_product():
+            rec._compute_purchase_price()
+        return rec
+
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    def _action_confirm(self):
+        res = super()._action_confirm()
+
+        # Explicitly recompute sale.order.line:purchase_price
+        self.mapped("order_line")._compute_purchase_price()
+
+        return res
