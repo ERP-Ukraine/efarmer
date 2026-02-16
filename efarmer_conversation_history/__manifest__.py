6eae6eeb22af7482c8cd52fffd333ac94b16fced
NadyaGornostay <chamamilla@tut.by>
2026-02-16 09:41:52 +0000

    [EF-403][ADD] efarmer_conversation_history module

---
+++ efarmer_conversation_history/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    "name": "Conversation History, eFarmer",
+    "version": "19.0.1.0.0",
+    "category": "Other",
+    "author": "ERP Ukraine, VentorTech",
+    "website": "https://erp.co.ua",
+    "support": "support@erp.co.ua",
+    "license": "OPL-1",
+    "auto_install": False,
+    "installable": True,
+    "application": True,
+    "demo": [],
+    "depends": [
+        "mail",
+        "crm",
+        "helpdesk",
+    ],
+    "data": [
+        "views/mail_message_views.xml",
+        "views/efarmer_conversation_history_menus.xml",
+    ],
+}
