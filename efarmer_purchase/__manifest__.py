48f08c7fea47442c51e564e01441034467c6ead9
Aleksandr-Mamonov <aleksandr.mamonov@ventor.tech>
2026-02-24 08:07:24 +0000

    [EF-403][MIGRATE] Copy efarmer_purchase module. Change version to 19.xxx

---
+++ efarmer_purchase/__manifest__.py
@@ -0,0 +1,31 @@
+# Copyright 2023 VentorTech OU
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+{
+    'name': 'eFarmer Purchase',
+    'version': '19.0.1.3.0',
+    'category': 'Other',
+    'author': 'VentorTech',
+    'website': 'https://ventor.tech',
+    'license': 'LGPL-3',
+    'depends': [
+        'base',
+        'purchase',
+        'purchase_advance_payment',
+        'mrp',
+        'hr',
+        'purchase_order_line_menu',
+    ],
+    'data': [
+        # Security
+        'security/security.xml',
+        # Model Views
+        'views/purchase_views.xml',
+        'views/res_company_views.xml',
+        'views/mrp_production_view.xml',
+        'views/res_user_views.xml',
+        'report/purchase_report_views.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
---
+++ efarmer_purchase/models/__init__.py
@@ -0,0 +1,6 @@
+# Copyright 2023 VentorTech OU
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+from . import purchase_order
+from . import res_company
+from . import res_user
