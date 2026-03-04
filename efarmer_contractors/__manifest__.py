f8c267014110fa296a21897578e7667cc39e078a
NadyaGornostay <chamamilla@tut.by>
2026-03-04 09:59:15 +0000

    [EF-403][ADD] efarmer_contractors module

---
+++ efarmer_contractors/__manifest__.py
@@ -0,0 +1,28 @@
+# Copyright 2026 VentorTech OU
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+{
+    "name": "eFarmer Invoice B2B Contactors",
+    "version": "19.0.1.0.0",
+    "author": "VentorTech",
+    "website": "https://ventor.tech/",
+    "license": "LGPL-3",
+    "category": "Services/Project",
+    "depends": [
+        "project",
+        "hr_timesheet",
+        "account",
+        "analytic",
+        "account_asset",
+        "efarmer_youtrack",
+        "hr",
+        "efarmer_timesheet",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/project_contractors_views.xml",
+        "views/project_contractors_line_views.xml",
+    ],
+    "installable": True,
+    "application": True,
+}
---
+++ efarmer_contractors/models/__init__.py
@@ -0,0 +1,4 @@
+# Copyright 2026 VentorTech OU
+# Part of Ventor modules. See LICENSE file for full copyright and licensing details.
+from . import project_contractors
+from . import project_contractors_line
