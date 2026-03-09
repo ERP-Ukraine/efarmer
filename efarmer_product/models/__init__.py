e8a13523b1e562b82c36bcd41f296f19662b159d
NadyaGornostay <chamamilla@tut.by>
2026-03-09 08:02:26 +0000

    [EF-403][ADD] efarmer_product module

--- efarmer_product/models/__init__.py
+++ efarmer_product/models/__init__.py
@@ -1,7 +1,16 @@
 # Copyright 2026 VentorTech OU
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
-from . import (account_fiscal_position, account_fiscal_position_tax,
-               product_function, product_template, product_vat, res_company,
-               res_config_settings, res_users, sale_order, sale_order_line,
-               stock_valuation_layer_revaluation)
+from . import (
+    # account_fiscal_position,
+    # account_fiscal_position_tax,
+    product_function,
+    product_template,
+    product_vat,
+    res_company,
+    res_config_settings,
+    res_users,
+    sale_order,
+    sale_order_line,
+    # stock_valuation_layer_revaluation,
+)
--- efarmer_product/models/sale_order_line.py
+++ efarmer_product/models/sale_order_line.py
@@ -20,38 +20,38 @@ class SaleOrderLine(models.Model):
         return super()._create(data_list)
 
     # OVERRIDE METHOD
-    def _compute_tax_id(self):
-        for line in self:
-            line = line.with_company(line.company_id)
-            fpos = (
-                line.order_id.fiscal_position_id
-                or line.order_id.fiscal_position_id.get_fiscal_position(
-                    line.order_partner_id.id
-                )
-            )
-            # This filter need for search goods products in SO and set all taxes like a goods product
-            # Task EF-182-change-doc-layout
-            default_product_func_id = (
-                self.env["ir.config_parameter"]
-                .sudo()
-                .get_param("product_function.product_function_id")
-            )
-            goods_product_lines = line.order_id.order_line.filtered(
-                lambda x: x.product_id.product_func_id.id
-                == int(default_product_func_id)
-            )
-            if goods_product_lines:
-                # If company_id is set, always filter taxes by the company
-                taxes = goods_product_lines[0].product_id.taxes_id.filtered(
-                    lambda t: t.company_id == line.env.company
-                )
-                line.tax_id = fpos.map_tax(taxes, goods_product_lines)
-            else:
-                # If company_id is set, always filter taxes by the company
-                taxes = line.product_id.taxes_id.filtered(
-                    lambda t: t.company_id == line.env.company
-                )
-                line.tax_id = fpos.map_tax(taxes, line)
+    # def _compute_tax_id(self):
+    #     for line in self:
+    #         line = line.with_company(line.company_id)
+    #         fpos = (
+    #             line.order_id.fiscal_position_id
+    #             or line.order_id.fiscal_position_id.get_fiscal_position(
+    #                 line.order_partner_id.id
+    #             )
+    #         )
+    #         # This filter need for search goods products in SO and set all taxes like a goods product
+    #         # Task EF-182-change-doc-layout
+    #         default_product_func_id = (
+    #             self.env["ir.config_parameter"]
+    #             .sudo()
+    #             .get_param("product_function.product_function_id")
+    #         )
+    #         goods_product_lines = line.order_id.order_line.filtered(
+    #             lambda x: x.product_id.product_func_id.id
+    #             == int(default_product_func_id)
+    #         )
+    #         if goods_product_lines:
+    #             # If company_id is set, always filter taxes by the company
+    #             taxes = goods_product_lines[0].product_id.taxes_id.filtered(
+    #                 lambda t: t.company_id == line.env.company
+    #             )
+    #             line.tax_id = fpos.map_tax(taxes, goods_product_lines)
+    #         else:
+    #             # If company_id is set, always filter taxes by the company
+    #             taxes = line.product_id.taxes_id.filtered(
+    #                 lambda t: t.company_id == line.env.company
+    #             )
+    #             line.tax_id = fpos.map_tax(taxes, line)
 
     def create(self, vals_list):
         lines = super().create(vals_list)
