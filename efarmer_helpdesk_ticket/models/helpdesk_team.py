6ae81f7221994ee1852239b809ae3b02f1039ba7
NadyaGornostay <chamamilla@tut.by>
2026-03-16 15:16:02 +0000

    [EF-403][ADD] efarmer_helpdesk and efarmer_helpdesk_ticket modules

---
+++ efarmer_helpdesk_ticket/models/helpdesk_team.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from odoo import api, fields, models
+
+
+class HelpdeskTeam(models.Model):
+    _inherit = "helpdesk.team"
+
+    use_short_form = fields.Boolean(string="Short Form")
+    allowed_tag_ids = fields.Many2many(
+        comodel_name="helpdesk.tag", string="Allowed Tags"
+    )
+
+    @api.onchange("use_product_repairs", "use_product_returns", "use_credit_notes")
+    def _onchange_use_short_form(self):
+        if (
+            not self.use_product_repairs
+            and not self.use_product_returns
+            and not self.use_credit_notes
+        ):
+            self.use_short_form = False
