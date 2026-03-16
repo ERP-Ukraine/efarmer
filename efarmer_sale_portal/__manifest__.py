452bcd224f89609400b2ee750e4b40e91a7d90c3
NadyaGornostay <chamamilla@tut.by>
2026-03-16 16:00:35 +0000

    [EF-403][ADD] efarmer_sale_portal module

---
+++ efarmer_sale_portal/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2026 VentorTech OU
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+{
+    "name": "eFarmer Sale Portal",
+    "version": "19.0.1.0.0",
+    "category": "Others",
+    "author": "VentorTech",
+    "website": "https://ventor.tech",
+    "license": "LGPL-3",
+    "depends": [
+        "sale",
+        "efarmer_sale_workflow",
+    ],
+    "description": "",
+    "data": [
+        "views/sale_portal_templates.xml",
+    ],
+    "auto_install": False,
+    "installable": True,
+    "application": True,
+}
---
+++ efarmer_sale_portal/controllers/portal.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from odoo.addons.sale.controllers import portal
+
+
+class CustomerPortal(portal.CustomerPortal):
+
+    def _prepare_quotations_domain(self, partner):
+        domain = [
+            ("message_partner_ids", "child_of", [partner.commercial_partner_id.id]),
+            ("state", "in", ["sent", "cancel", "to_payment", "to_confirm"]),
+        ]
+        return domain
