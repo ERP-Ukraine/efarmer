f4ebd02bb59a06bb94c03cff9ab13332c3f6d415
NadyaGornostay <chamamilla@tut.by>
2026-04-14 14:26:13 +0000

    [EF-403][ADD]  efarmer_sold_products_and_lots module

--- efarmer_sold_products_and_lots/__manifest__.py
+++ efarmer_sold_products_and_lots/__manifest__.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'eFarmer Sold Products and Lots',
-    'version': '19.0.1.0.0',
+    'version': '18.0.1.0.0',
     'category': 'Inventory',
     'author': 'VentorTech',
     'website': 'https://ventor.tech/',
     'license': 'LGPL-3',
-    'depends': ['stock_barcode'],
+    'depends': ['stock', 'stock_barcode'],
     'description': '',
     'data': [
         'views/stock_views.xml',
