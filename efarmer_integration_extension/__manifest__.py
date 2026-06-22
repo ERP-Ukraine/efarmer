b6cbc59f74b814d53b6d22c594c9b9b5f0b7d65c
NadyaGornostay <chamamilla@tut.by>
2026-06-22 09:23:49 +0000

    [EF-403][ADD] visibility for Cancel button in SO

---
+++ efarmer_integration_extension/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    "name": "Integration Extension, eFarmer",
+    "version": "1.0",
+    "category": "Other",
+    "author": "ERP Ukraine",
+    "website": "https://erp.co.ua",
+    "support": "support@erp.co.ua",
+    "license": "OPL-1",
+    "auto_install": False,
+    "installable": True,
+    "application": False,
+    "demo": [],
+    "depends": [
+        "integration",
+        "efarmer_sale_workflow",
+    ],
+    "data": [
+        "views/sale_order_views.xml",
+    ],
+}
